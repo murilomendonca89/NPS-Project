2e629fe6ddd6babb2be2f760bd8c25bc8df6574c
murilomendonca89 <murilomendonca@live.com>
2021-04-16 04:29:24 -0300

    survey migrations, surveys repository, surveys controller

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express'
-import { getRepository } from 'typeorm'
-import { User } from '../models/User'
+import { getCustomRepository } from 'typeorm'
+import { UserRepository } from '../repositories/UsersRepository'
 
 class UserController {
     async create(req: Request, res: Response) {
         const { name, email } = req.body
         
-        const userRepositiry = getRepository(User)
+        const userRepositiry = getCustomRepository(UserRepository)
 
         const userAlreadyExists = await userRepositiry.findOne({
             email
--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,5 @@ app.use(express.json())
 
 app.use(router)
 
+
 app.listen(3333, () => console.log("Server is running!"))
